f2147f7f1b5e0b6782fcfcde221fb3a9f491f16f
Alicia Wood <awood@adobe.com>
2017-06-08 20:49:08 -0600

    Get live reloading!

--- server.js
+++ server.js
@@ -1,35 +1,37 @@
-const express = require('express');
-const webpack = require('webpack');
-const config = require('./webpack.config');
-var path = require('path');
-var DIST_DIR = path.join(__dirname, 'src');
+var express = require('express'),
+	webpack = require('webpack'),
+	webpackDevMiddleware = require('webpack-dev-middleware'),
+	webpackHotMiddleware = require('webpack-hot-middleware'),
+	config = require('./webpack.config'),
+	path = require('path');
+	
 
 /* Runs webpack dev server to package and serve the app */
 
-const app = express();
-const compiler = webpack(config);
+const app = express(),
+	DIST_DIR = path.join(__dirname, 'src'),
+	PORT = 3000,
+	compiler = webpack(config);
 
-// app.use(require('webpack-dev-middleware')(compiler, {
-//   noInfo: true,
-//   publicPath: config.output.path
-// }));
+app.use(webpackDevMiddleware(compiler, {  
+    publicPath: config.output.publicPath
+}));
 
-// app.use(require('webpack-hot-middleware')(compiler));
+app.use(webpackHotMiddleware(compiler));
 
 app.use(express.static(DIST_DIR));
 
-// app.get("*", (req, res) => res.sendFile(express.static(__dirname + '/src/index.html')));
-app.get("*", function (req, res) {  
-  res.sendFile(path.join(DIST_DIR, "index.html"));
+app.get("*", (req, res, next) => {  
+    const filename = path.join(DIST_DIR, "index.html");
+
+    compiler.outputFileSystem.readFile(filename, (err, result) => {
+	    if (err) {
+	        return next(err);
+	    }
+	    res.set('content-type', 'text/html');
+	    res.send(result);
+	    res.end();
+    });
 });
 
-app.listen(3000, 'localhost', function(err) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log('Listening at http://localhost:3000');
-});
-
-
-
+app.listen(PORT);  
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
 var webpack = require('webpack');
 var path = require('path');
-
-
+var webpackHotMiddleware = require('webpack-hot-middleware');
 var BUILD_DIR = path.resolve(__dirname, 'src/public');
 var APP_DIR = path.resolve(__dirname, 'src/app');
 
 var config = {
   context: APP_DIR,
-  entry: './index',
+  entry: ['webpack-hot-middleware/client','./index'],
   output: {
     path: BUILD_DIR,
     publicPath: '/',
     filename: 'bundle.js'
   },
+  plugins: [
+   new webpack.optimize.OccurrenceOrderPlugin(),
+   new webpack.HotModuleReplacementPlugin(),
+   new webpack.NoEmitOnErrorsPlugin()
+  ],
   module : {
     loaders : [
       {
