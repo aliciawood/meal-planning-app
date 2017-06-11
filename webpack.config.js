24e9ab6a59ccf6587a7887b30f1993dab7aefc0b
Alicia Wood <awood@adobe.com>
2017-06-10 22:42:25 -0600

    add css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,51 @@
 var webpack = require('webpack');
 var path = require('path');
 var webpackHotMiddleware = require('webpack-hot-middleware');
+var combineLoaders = require('webpack-combine-loaders');
 var BUILD_DIR = path.resolve(__dirname, 'src/public');
 var APP_DIR = path.resolve(__dirname, 'src');
 
 var config = {
-  context: APP_DIR,
-  entry: ['webpack-hot-middleware/client','./index'],
-  output: {
-    path: BUILD_DIR,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  plugins: [
-   new webpack.optimize.OccurrenceOrderPlugin(),
-   new webpack.HotModuleReplacementPlugin(),
-   new webpack.NoEmitOnErrorsPlugin()
-  ],
-  module : {
-    loaders : [
-      {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        exclude: '/node_modules/',
-        loader : 'babel-loader',
-        query: {
-          presets: ['es2015']
+	context: APP_DIR,
+	entry: ['webpack-hot-middleware/client','./index'],
+  	output: {
+    	path: BUILD_DIR,
+    	publicPath: '/',
+    	filename: 'bundle.js'
+  	},
+  	plugins: [
+   		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoEmitOnErrorsPlugin()
+  	],
+  	module : {
+    	loaders : [
+      	{
+        	test : /\.jsx?/,
+        	include : APP_DIR,
+        	exclude: '/node_modules/',
+        	loader : 'babel-loader',
+        	query: {
+          		presets: ['es2015']
+        	}
+      	},
+       	{
+        	test: /\.css$/,
+          	loader: combineLoaders([
+	        	{
+	            	loader: 'style-loader'
+	            }, 
+	            {
+	            	loader: 'css-loader',
+	            	query: {
+	                	modules: true,
+	                	localIdentName: '[name]__[local]___[hash:base64:5]'
+	              	}
+	            }
+          	])
         }
-      }
-    ]
-  }
+    	]
+  	}
 };
 
 module.exports = config;
