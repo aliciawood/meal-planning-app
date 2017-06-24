4e8ec8a009aecc03c2291ae9068dd2820c59e9ba
Alicia Wood <awood@adobe.com>
2017-06-24 08:24:28 -0600

    example test file

---
+++ ExampleTest.test.js
@@ -0,0 +1,15 @@
+// test('will render hello', () => {
+//   expect('hello').toBe('hello');
+// });
+
+// describe('describe test suite here',()=>{
+
+//     beforeEach(()=>{        
+        
+//     })
+
+//     it('will be a karma test', () => {
+
+//     });
+
+// });
